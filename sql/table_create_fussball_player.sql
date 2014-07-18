9fc23828ab0cad89fa008d547995b05951e6e0e1
Lars Nielsen <larn@tv2.dk>
2014-07-18 13:27:48 +0200

    Added unique key to player name

--- sql/table_create_fussball_player.sql
+++ sql/table_create_fussball_player.sql
@@ -3,5 +3,6 @@ CREATE TABLE `player` (
   `name` varchar(128) COLLATE utf8_danish_ci DEFAULT NULL,
   `player_score` int(11) NOT NULL DEFAULT '1500',
   `modified` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`player_id`)
+  PRIMARY KEY (`player_id`),
+  UNIQUE KEY `UNIQUE_PLAYER_NAME` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
