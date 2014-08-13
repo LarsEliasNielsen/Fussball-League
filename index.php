f1a7b85f799924b5bb7339ce201e2f4ff295cb51
Lars Nielsen <lars@lndesign.dk>
2014-08-13 20:52:10 +0200

    Added Foundation 5

--- index.php
+++ index.php
@@ -10,19 +10,29 @@
   // print_r($players);
 
 ?>
-
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
 <html>
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register game</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/foundation.css">
+    <style>
+      * { margin: 0; padding: 0; }
+      .columns { border: 1px solid gray; }
+    </style>
   </head>
 
   <body>
     <h1>Register a game</h1>
 
-    <form>
-      <div>
-        <div id="red" class="team team-red">
-          <h3>Red</h3>
+    <div class="row">
+
+      <div class="small-6 columns">
+        <h3>Red</h3>
           <select id="red-palyer-1">
             <option>Select red player</option>
             <?php
@@ -35,9 +45,10 @@
               echo $player_options;
             ?>
           </select>
-        </div>
-        <div id="blue" class="team team-blue">
-          <h3>Blue</h3>
+      </div>
+
+      <div class="small-6 columns">
+        <h3>Blue</h3>
           <select id="blue-palyer-1">
             <option>Select blue player</option>
             <?php
@@ -50,9 +61,14 @@
               echo $player_options;
             ?>
           </select>
-        </div>
       </div>
-    </form>
 
+    </div>
+
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
   </body>
 </html>
